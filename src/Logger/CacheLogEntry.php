1ab93cb3371d6279c1aac9b3b259054f4ea59350
fly2abhishek <fly2abhishek@468982.no-reply.drupal.org>
2016-04-04 13:03:41 +0530

    Issue #2695175 by abhishek-anand: Port CacheLogEntry and DatabaseLogEntry to Drupal 8

--- src/Logger/CacheLogEntry.php
+++ src/Logger/CacheLogEntry.php
@@ -6,7 +6,12 @@
 namespace Drupal\ultimate_cron\Logger;
 
 
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\ultimate_cron\Entity\CronJob;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class CacheLogEntry extends LogEntry {
+
   /**
    * Save log entry.
    */
@@ -15,16 +20,14 @@ class CacheLogEntry extends LogEntry {
       return;
     }
 
-    if ($this->log_type != ULTIMATE_CRON_LOG_TYPE_NORMAL) {
-      return;
-    }
-
-    $job = ultimate_cron_job_load($this->name);
-
-    $settings = $job->getSettings('logger');
+    $settings = $this->logger->getConfiguration();
 
     $expire = $settings['timeout'] > 0 ? time() + $settings['timeout'] : $settings['timeout'];
-    cache_set('uc-name:' . $this->name, $this->lid, $settings['bin'], $expire);
-    cache_set('uc-lid:' . $this->lid, $this->getData(), $settings['bin'], $expire);
+
+    \Drupal::cache($settings['bin'])
+      ->set('uc-name:' . $this->name, $this->lid, $expire);
+    \Drupal::cache($settings['bin'])
+      ->set('uc-lid:' . $this->lid, $this->getData(), $expire);
+
   }
 }
--- src/Plugin/ultimate_cron/Logger/CacheLogger.php
+++ src/Plugin/ultimate_cron/Logger/CacheLogger.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\ultimate_cron\Plugin\ultimate_cron\Logger;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\ultimate_cron\Logger\LoggerBase;
 
@@ -28,8 +29,8 @@ class CacheLogger extends LoggerBase {
    */
   public function defaultConfiguration() {
     return array(
-      'bin' => 'cache_ultimate_cron',
-      'timeout' => 0,
+      'bin' => 'ultimate_cron_logger',
+      'timeout' => Cache::PERMANENT,
     );
   }
 
@@ -38,18 +39,16 @@ class CacheLogger extends LoggerBase {
    */
   public function load($name, $lock_id = NULL, array $log_types = [ULTIMATE_CRON_LOG_TYPE_NORMAL]) {
     $log_entry = new $this->logEntryClass($name, $this);
-
-    $job = ultimate_cron_job_load($name);
-    $settings = $job->getSettings('logger');
+    $settings = $log_entry->logger->configuration;
 
     if (!$lock_id) {
-      $cache = cache_get('uc-name:' . $name, $settings['bin']);
+      $cache =  \Drupal::cache($settings['bin'])->get('uc-name:' . $name, TRUE);
       if (empty($cache) || empty($cache->data)) {
         return $log_entry;
       }
       $lock_id = $cache->data;
     }
-    $cache = cache_get('uc-lid:' . $lock_id, $settings['bin']);
+    $cache = \Drupal::cache($settings['bin'])->get('uc-lid:' . $lock_id, TRUE);
 
     if (!empty($cache->data)) {
       $log_entry->setData((array) $cache->data);
@@ -94,7 +93,7 @@ class CacheLogger extends LoggerBase {
     $form['timeout'] = [
       '#type' => 'textfield',
       '#title' => t('Cache timeout'),
-      '#description' => t('Seconds before cache entry expires (0 = never, -1 = on next general cache wipe).'),
+      '#description' => t('Seconds before cache entry expires (0 = never).'),
       '#default_value' => $config->get('logger.cache.timeout'),
       '#fallback' => TRUE,
       '#required' => TRUE,
